acee9684889ba4f5693514ac6b5577e6aefbe2e7
Mujo Osmanovic <mosmanovic@Mujos-Workstation.local>
2024-07-28 00:27:46 +0200

    update

--- src/app/components/stan/suteren.ts
+++ src/app/components/stan/suteren.ts
@@ -8,55 +8,55 @@ export const stanovi = [{
 }, {
   name: 'G.2',
   stan: '2',
-  stanje: 'slobodan',
+  stanje: 'prodat',
   kvadrature: [
     { name: 'GARAZA', size: 13.72 },
-  ]
-}, {
-  name: 'G.3',
-  stan: '3',
-  stanje: 'slobodan',
-  kvadrature: [
-    { name: 'GARAZA', size: 15.99 },
   ]
 }, {
   name: 'G.4',
   stan: '4',
   stanje: 'slobodan',
   kvadrature: [
-    { name: 'GARAZA', size: 17.55 },
+    { name: 'GARAZA', size: 15.99 },
   ]
 }, {
   name: 'G.5',
   stan: '5',
-  stanje: 'slobodan',
+  stanje: 'prodat',
   kvadrature: [
-    { name: 'GARAZA', size: 13.94 },
+    { name: 'GARAZA', size: 17.55 },
   ]
 }, {
   name: 'G.6',
   stan: '6',
   stanje: 'slobodan',
   kvadrature: [
-    { name: 'GARAZA', size: 16.70 },
+    { name: 'GARAZA', size: 13.94 },
   ]
 }, {
   name: 'G.7',
   stan: '7',
-  stanje: 'slobodan',
+  stanje: 'prodat',
   kvadrature: [
-    { name: 'GARAZA', size: 16.05 },
+    { name: 'GARAZA', size: 16.70 },
   ]
 }, {
   name: 'G.8',
   stan: '8',
-  stanje: 'slobodan',
+  stanje: 'prodat',
   kvadrature: [
-    { name: 'GARAZA', size: 12.36 },
+    { name: 'GARAZA', size: 16.05 },
   ]
 }, {
   name: 'G.9',
   stan: '9',
+  stanje: 'prodat',
+  kvadrature: [
+    { name: 'GARAZA', size: 12.36 },
+  ]
+}, {
+  name: 'G.10',
+  stan: '10',
   stanje: 'slobodan',
   kvadrature: [
     { name: 'GARAZA', size: 14.42 },
